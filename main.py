0418e124f1f0685372a4e084d70162dbf5923599
Jason Hsu <github@jasonhsu.com>
2012-02-09 11:44:49 -0600

    Initial commit (LightDM)

---
+++ main.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+# Check for root user login
+import os, sys
+if not os.geteuid()==0:
+    sys.exit("\nOnly root can run this script\n")
+
+# Get your username (not root)
+import pwd
+uname=pwd.getpwuid(1000)[0]
+
+# The remastering process uses chroot mode.
+# Check to see if this script is operating in chroot mode.
+# /home/mint directory only exists in chroot mode
+is_chroot = os.path.exists('/home/mint')
+dir_develop=''
+if (is_chroot):
+	dir_develop='/usr/local/bin/develop'
+	dir_user = '/home/mint'
+else:
+	dir_develop='/home/' + uname + '/develop'
+	dir_user = '/home/' + uname
+
+# Everything up to this point is common to all Python scripts called by shared-*.sh
+# =================================================================================
+
+# THIS IS THE SCRIPT FOR REPLACING THE DEFAULT LMDE DISPLAY MANAGER WITH A LIGHTER ONE.
+
+# Remove GDM
+os.system('apt-get purge -y gdm3')
+
+# Install LightDM
+os.system('apt-get install -y lightdm')
+
+# ============================
+# Configure LightDM
+import shutil
+
+
